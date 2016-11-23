35407ff14d87d84b955db556d49a7e46edcbf613
Michael Trimm <michael@michaeltrimm.com>
2016-11-23 12:33:04 -0500

    Added new map form

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -8,4 +8,36 @@ class MapController < ApplicationController
     
   end
   
+  def create
+    
+  end
+  
+  def new 
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def show 
+    map_id = params[:map_id]
+    map = Map.where(user_id: current_user.id, id: map_id)
+    map.inspect
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+  def map_params
+    params.require(:map).permit(:upper_right, :lower_left)
+  end
+  
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -2,4 +2,8 @@ class Map
   include Mongoid::Document
   has_many :mapmarkers
   belongs_to :user
+  
+  field :name, type: String
+  field :upper_right, type: Hash
+  field :lower_left, type: Hash
 end
